67c572797f89a9acce0d224eaf7b0b437e7e06f0
Antony Blakey <antony@soops.nl>
2019-03-18 09:57:50 +0000

    Add subcommand mechanism to split functionality

--- src/main.rs
+++ src/main.rs
@@ -16,95 +16,82 @@ use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
-struct Opt {
-    /// Directories containing configuration
-    #[structopt(parse(from_os_str))]
-    dirs: Vec<PathBuf>,
-
+struct Args {
     #[structopt(flatten)]
     verbosity: clap_verbosity_flag::Verbosity,
+
+    #[structopt(subcommand)]
+    command: Command,
 }
 
-fn get_window_name(connection: &xcb::base::Connection, window: xcb::xproto::Window) -> String {
-    let atom_utf8_string = xcb::xproto::intern_atom(&connection, true, "UTF8_STRING")
-        .get_reply()
-        .unwrap()
-        .atom();
-    let property = xcb::xproto::get_property(
-        connection,
-        false,
-        window,
-        xcb::xproto::ATOM_WM_NAME,
-        atom_utf8_string,
-        0,
-        256,
-    )
-    .get_reply()
-    .unwrap();
-    String::from(std::str::from_utf8(property.value()).unwrap())
+#[derive(Debug, StructOpt)]
+enum Command {
+    /// listen for key commands, showing help as appropriate
+    #[structopt(name="listen")]
+    Listen(ListenCommand),
+
+    /// Select a window by showing key overlays
+    #[structopt(name="select")]
+    Select(SelectCommand),
 }
 
-fn window_is_selectable(connection: &xcb::base::Connection, window: xcb::xproto::Window) -> bool {
-    let is_viewable = xcb::xproto::get_window_attributes(&connection, window)
-        .get_reply()
-        .unwrap()
-        .map_state()
-        == xcb::xproto::MAP_STATE_VIEWABLE as u8;
-    is_viewable && !get_window_name(connection, window).is_empty()
+#[derive(Debug, StructOpt)]
+struct ListenCommand {
+    /// Directories containing configuration
+    #[structopt(parse(from_os_str))]
+    config: Vec<PathBuf>,
 }
 
-fn selectable_windows(connection: &xcb::base::Connection) -> Vec<xcb::xproto::Window> {
-    let screen = connection.get_setup().roots().nth(0).unwrap();
-    let query = xcb::xproto::query_tree(&connection, screen.root())
-        .get_reply()
-        .unwrap();
-    query
-        .children()
-        .into_iter()
-        .map(|&w| w)
-        .filter(|&w| window_is_selectable(connection, w))
-        .collect::<Vec<_>>()
+#[derive(Debug, StructOpt)]
+struct SelectCommand {
+    /// Files containing configuration
+    #[structopt(parse(from_os_str))]
+    config: Vec<PathBuf>,
 }
 
 fn main() {
-    let args = Opt::from_args();
+    let args = Args::from_args();
     args.verbosity.setup_env_logger("commando").unwrap();
 
     let (connection, screen_number) = xcb::Connection::connect(None).unwrap();
 
     let keysource = KeySource::new(&connection, screen_number);
 
-    let files: Vec<PathBuf> = args
-        .dirs
-        .iter()
-        .filter(|d| d.is_dir())
-        .map(|d| d.canonicalize().unwrap())
-        .flat_map(|d| d.read_dir().unwrap())
-        .map(|e| e.unwrap().path())
-        .filter(|d| d.is_file())
-        .collect();
+    match args.command {
+        Command::Listen(ListenCommand { config }) => {
+            let files: Vec<PathBuf> = config
+                .iter()
+                .filter(|d| d.is_dir())
+                .map(|d| d.canonicalize().unwrap())
+                .flat_map(|d| d.read_dir().unwrap())
+                .map(|e| e.unwrap().path())
+                .filter(|d| d.is_file())
+                .collect();
 
-    let css: Vec<PathBuf> = files
-        .iter()
-        .filter(|f| f.file_name().unwrap() == "help.css")
-        .map(|f| f.clone())
-        .collect();
-    let javascript: Vec<PathBuf> = files
-        .iter()
-        .filter(|f| f.file_name().unwrap() == "help.js")
-        .map(|f| f.clone())
-        .collect();
-    let definitions = files
-        .iter()
-        .filter(|f| {
-            let ext = f.extension().unwrap();
-            ext == "json5" || ext == "json"
-        })
-        .map(|f| std::fs::read_to_string(f).unwrap())
-        .map(|source| ConfigFile::from_string(&source).unwrap())
-        .flat_map(|config| config.definitions);
+            let css: Vec<PathBuf> = files
+                .iter()
+                .filter(|f| f.file_name().unwrap() == "help.css")
+                .map(|f| f.clone())
+                .collect();
+            let javascript: Vec<PathBuf> = files
+                .iter()
+                .filter(|f| f.file_name().unwrap() == "help.js")
+                .map(|f| f.clone())
+                .collect();
+            let definitions = files
+                .iter()
+                .filter(|f| {
+                    let ext = f.extension().unwrap();
+                    ext == "json5" || ext == "json"
+                })
+                .map(|f| std::fs::read_to_string(f).unwrap())
+                .map(|source| ConfigFile::from_string(&source).unwrap())
+                .flat_map(|config| config.definitions);
 
-    let model = Model::new(definitions, css, javascript, &keysource);
+            let model = Model::new(definitions, css, javascript, &keysource);
 
-    Interpreter::run(&model, &keysource);
+            Interpreter::run(&model, &keysource);
+        }
+        Command::Select(SelectCommand { config: _ }) => {}
+    }
 }
