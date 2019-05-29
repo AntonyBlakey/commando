a69d72994af4f3f673449c5df9105dd7355e4e1b
Antony Blakey <antony@soops.nl>
2019-05-29 16:41:52 +0200

    Minor fixes

--- src/ceramic.rs
+++ src/ceramic.rs
@@ -14,7 +14,7 @@ pub fn extend_model(model: &mut Model) {
         root {
             Cmd + space => { "Cycle Layout" ceramic_do("switch_to_next_layout") }
             Cmd + Opt + q => { "Exit" ceramic_do("quit") }
-            // Cmd + Backspace => { "Close Window" {} }
+            Cmd + Backspace => { "Close Window" ceramic_do("close_focused_window") }
             Cmd + minus => { "Decrease Ratio" ceramic_do("layout/decrease_ratio") }
             Cmd + plus => { "Increase Ratio" ceramic_do("layout/increase_ratio") }
             Cmd + Opt + minus => { "Decrease Count" ceramic_do("layout/decrease_count") }
@@ -39,15 +39,15 @@ pub fn extend_model(model: &mut Model) {
             group "Move" {
                 Cmd + Shift + j => { "Forward" ceramic_do("move_focused_window_forward") }
                 Cmd + Shift + k => { "Backward" ceramic_do("move_focused_window_backward") }
-                // Cmd + Shift + 1 => { "To Space 1" {} }
-                // Cmd + Shift + 2 => { "To Space 2" {} }
-                // Cmd + Shift + 3 => { "To Space 3" {} }
-                // Cmd + Shift + 4 => { "To Space 4" {} }
-                // Cmd + Shift + 5 => { "To Space 5" {} }
-                // Cmd + Shift + 6 => { "To Space 6" {} }
-                // Cmd + Shift + 7 => { "To Space 7" {} }
-                // Cmd + Shift + 8 => { "To Space 8" {} }
-                // Cmd + Shift + 9 => { "To Space 9" {} }
+                Cmd + Shift + 1 => { "To Space 1" ceramic_do("move_focused_window_to_workspace_named: 1") }
+                Cmd + Shift + 2 => { "To Space 2" ceramic_do("move_focused_window_to_workspace_named: 2") }
+                Cmd + Shift + 3 => { "To Space 3" ceramic_do("move_focused_window_to_workspace_named: 3") }
+                Cmd + Shift + 4 => { "To Space 4" ceramic_do("move_focused_window_to_workspace_named: 4") }
+                Cmd + Shift + 5 => { "To Space 5" ceramic_do("move_focused_window_to_workspace_named: 5") }
+                Cmd + Shift + 6 => { "To Space 6" ceramic_do("move_focused_window_to_workspace_named: 6") }
+                Cmd + Shift + 7 => { "To Space 7" ceramic_do("move_focused_window_to_workspace_named: 7") }
+                Cmd + Shift + 8 => { "To Space 8" ceramic_do("move_focused_window_to_workspace_named: 8") }
+                Cmd + Shift + 9 => { "To Space 9" ceramic_do("move_focused_window_to_workspace_named: 9") }
                 // Cmd + Shift + 0 => { "To …" {} }
                 // Cmd + Shift + Opt + 0 => { "Swap With …" {} }
                 Cmd + Opt + 0 => { "Pull To Head …" ceramic_do("move_focused_window_to_head") }
